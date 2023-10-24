cb435e3f34139401c93ed3e4725c347c63665be5
vijai-ks <vijai.ksv@gmail.com>
2023-10-24 09:23:20 +0530

    feat: loading_screen: Add Http package and test
    Signed-off-by: vijai-ks <vijai.ksv@gmail.com>

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -19,6 +20,14 @@ class _LoadingScreenState extends State<LoadingScreen> {
     Location location = Location();
     await location.getCurrentLocation();
     print(location.longitude);
+
+    http.Response response = await http.get(
+      Uri.parse(
+          'https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b1b15e88fa797225412429c1c50c122a1'),
+    );
+
+    print(response.statusCode);
+    print(response.body);
   }
 
   @override
